23887a31a998e1ba6843e0780df92702af572f3b
swpinkerton <98047222+swpinkerton@users.noreply.github.com>
2022-11-28 22:35:46 +0000

    Add files via upload

---
+++ cmake-build-debug/Google_Tests/library/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Workspace/UGRtest/Google_Tests/library/googletest
+# Build directory: C:/Workspace/UGRtest/cmake-build-debug/Google_Tests/library/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/Google_Tests/library/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Workspace/UGRtest/Google_Tests/library/googletest/googletest
+# Build directory: C:/Workspace/UGRtest/cmake-build-debug/Google_Tests/library/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
