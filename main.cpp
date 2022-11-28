e0551be985931815d56047014d97fa69027ad4f2
swpinkerton <98047222+swpinkerton@users.noreply.github.com>
2022-11-28 22:34:07 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Function_lib/Function.cpp"
+
+int main() {
+    auto bps = 1;
+    auto result = function(bps);
+    std::cout << "Hello, World! " << std::endl;
+    return result;
+}
